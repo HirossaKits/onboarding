9801bfe37c42aeb0f3a261a499686a2a0a4b8e0b
Hirohisa Kitsuka <hi.kitsuka@gmail.com>
2022-09-03 22:42:20 +0900

    Add Config for air

--- backend/internal/middleware/auth_middleware.go
+++ backend/internal/middleware/auth_middleware.go
@@ -8,8 +8,10 @@ import (
 func AuthMiddleWare(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 
+		message := "Authentificated"
+
 		// ここで認証の処理を行う
-		fmt.Println("Authentificated")
+		fmt.Println(message)
 
 		next.ServeHTTP(w, r)
 	})
